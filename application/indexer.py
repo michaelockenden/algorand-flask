b186db9d4bd6bcb165d38de1468152d6ca19e227
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-09-28 18:37:06 +0100

    Update indexer.py

--- application/indexer.py
+++ application/indexer.py
@@ -7,7 +7,7 @@ def myindexer():
 
     algod_address = "https://testnet-algorand.api.purestake.io/idx2"
     # FIXME: Put your API key in
-    algod_token = "aPubZNdg2Y10ADA7lnMoUa7UO808BfneaneiddG0 "
+    algod_token = "aPubZNdg2Y10ADA7lnMoUa7UO808BfneaneiddG0"
 
     headers = {
         "X-API-Key": algod_token,
